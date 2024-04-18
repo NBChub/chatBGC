cbc0562bb8ef00add508f6e6d0f3110b787cd723
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2024-04-18 12:56:38 +0000

    feat: add example vanna app

--- chatbgc/app.py
+++ chatbgc/app.py
@@ -1,1 +1,11 @@
 """Main module."""
+
+import vanna
+from vanna.flask import VannaFlaskApp
+from vanna.remote import VannaDefault
+
+
+def start_app(email, sqlite_path="https://vanna.ai/Chinook.sqlite"):
+    vn = VannaDefault(model="chinook", api_key=vanna.get_api_key(email))
+    vn.connect_to_sqlite(sqlite_path)
+    VannaFlaskApp(vn).run()
