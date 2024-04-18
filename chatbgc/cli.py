c7f7ec5a9c1332edf4b4387e4f39f7d28ad2feb1
Matin Nuhamunada <matinnu@biosustain.dtu.dk>
2024-04-18 12:56:38 +0000

    feat: add example vanna app

--- chatbgc/cli.py
+++ chatbgc/cli.py
@@ -2,17 +2,55 @@
 
 import fire
 
-
-def help() -> None:
-    print("chatbgc")
-    print("=" * len("chatbgc"))
-    print(
-        "Ask questions about biosynthetic gene clusters in your genome collection via LLMs using Retrieval-Augmented Generation (RAG)."
-    )
+from .app import start_app
 
 
-def main() -> None:
-    fire.Fire({"help": help})
+class CLI(object):
+    """
+    Command-line interface for chatBGC.
+
+    chatBGC is a tool for asking questions about biosynthetic gene clusters in your genome collection via LLMs using Retrieval-Augmented Generation (RAG).
+
+    Attributes:
+        None
+    """
+
+    def __init__(self):
+        """
+        The constructor for CLI class.
+
+        Parameters:
+            None
+        """
+        pass
+
+    def run(self, email, sqlite_path="https://vanna.ai/Chinook.sqlite"):
+        """
+        Starts the chatBGC interface using the vanna.ai library.
+
+        The run method is used to start the chatBGC tool. It connects to a SQLite database using the vanna.ai library and starts a Flask app.
+
+        Parameters:
+            email (str): The email to use for retrieving the API key from vanna.ai.
+            sqlite_path (str, optional): The URL or path to the SQLite database. Defaults to 'https://vanna.ai/Chinook.sqlite'.
+
+        Returns:
+            None
+        """
+        start_app(email, sqlite_path=sqlite_path)
+
+
+def main():
+    """
+    The main function is used to start the CLI for chatBGC.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
+    fire.Fire(CLI)
 
 
 if __name__ == "__main__":
